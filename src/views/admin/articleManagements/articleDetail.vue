2befb18736d23df1a6943480114ef52f93f27b5c
丛紫怡 <congziyi@xhd.cn>
2019-11-14 09:23:34 +0800

    articeDetail

--- src/views/admin/articleManagements/articleDetail.vue
+++ src/views/admin/articleManagements/articleDetail.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="detail-box">
     <div class='detail-container'>
+      <div class="title">
+        <h1>{{dataInfo.title}}</h1>
+      </div>
+      <div class="up-title">
+        <span>作者：{{dataInfo.author}}</span>
+        <span>创建时间：{{dataInfo.createdAt}}</span>
+        <span>分类：{{dataInfo.sort}}</span>
+        <span>类型：{{dataInfo.type == 1 ? '图文':'视频'}}</span>
+      </div>
       <div v-html="dataInfo.content"></div>
     </div>
   </div>
@@ -35,8 +44,25 @@ export default {
     width: 100%;
     height: 100;
     .detail-container {
+      padding: 20px;
       width: 80%;
       margin:  0 auto;
+      background: #fff;
+      .title{
+        width: 100%;
+        height: 50px;
+        text-align: center;
+      }
+      .up-title{
+        width: 100%;
+        height: 50px;
+        text-align: center;
+        font-size: 16px;
+        color: #ccc;
+        span{
+          margin: 0 10px;
+        }
+      }
     }
   }
 </style>
